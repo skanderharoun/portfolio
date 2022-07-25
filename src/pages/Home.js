c2598ddbe5a0c4a2806296a47982d912ade624e5
Iskander Belhadjkhlifa <YourEmailAddress>
2022-07-25 15:56:03 +0200

    Add content to the Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import webImage from '../images/photo-2.png'
 function Home() {
   return (
-    <div className="alert alert-info mt-5" role="alert">
-      Hello my name is Iskander 
-      I am a Fullstack web Developer
+    <div className="alert alert-info mt-5 container" role="alert">
+      <img className='with-border-radius' src='/images/photo-1.jpg' width='150px' alt='my photo'/>
+     <p> Hello my name is Iskander 
+      I am a Fullstack web Developer</p>
+      <img src={webImage} style={{width:'100%'}} alt="web" />
     </div>
   )
 }
