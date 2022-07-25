de0a303d87c3040cc85a4c68450a8868369ddbd3
Iskander Belhadjkhlifa <YourEmailAddress>
2022-07-25 16:29:29 +0200

    install react-icons and set up the contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import { AiOutlineMail } from 'react-icons/ai';
+import { BsFillPhoneFill } from 'react-icons/bs';
+import { FaMapMarker } from 'react-icons/fa';
+
+
+const contactInformations = {
+  email: 'skander_haj@hotmail.fr',
+  phone: "+21698668889",
+  address: {
+    street: "route taniour km 7",
+    city: 'sfax',
+    country: 'Tunisie'
+
+  }
+}
 
 function Contact() {
   return (
-    <div>Contact</div>
+
+    <div className='container mt-5'>
+      <div>
+        < AiOutlineMail size={40} />
+        <span style={{ marginLeft: 6 }}>{contactInformations.email}</span>
+      </div>
+      <div className='mt-3'>
+        < BsFillPhoneFill size={40} />
+        <span style={{ marginLeft: 6 }}>{contactInformations.phone}</span>
+      </div>
+      <div className='mt-3'>
+        < FaMapMarker size={40} />
+        <span style={{ marginLeft: 6 }}>{contactInformations.address.street},{contactInformations.address.city},{contactInformations.address.country}</span>
+      </div>
+    </div>
   )
 }
 
