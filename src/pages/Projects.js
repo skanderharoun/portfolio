8794f4cfafd02d2b0eea7fd7414be1b245d02d89
Iskander Belhadjkhlifa <YourEmailAddress>
2022-07-25 18:05:37 +0200

    set up th skills page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -17,11 +17,11 @@ const projects = [
 ]
 function Projects() {
   return (
-    <div className='mt-5 container'>
+    <div className='mt-5 container' >
       <div className='row'>
       {projects.map((item) => {
         return (
-          <div className="card col-12 col-md-6" >
+          <div className="card col-12 col-md-6" key={item.githubId}>
             <div className="card-body">
               <h5 className="card-title">{item.title}</h5>
               <h6 className="card-subtitle mb-2 text-muted">{item.githubId}</h6>
